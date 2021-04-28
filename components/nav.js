8f05b720dca155d780deebad5b332b5f71361038
tabsontabs <tabithapoeze@gmail.com>
2021-04-28 17:17:26 -0400

    updated nav testing

--- components/nav.js
+++ components/nav.js
@@ -1,50 +1,50 @@
-import { useEffect, useState } from 'react'
-import NavLink from '../components/navLink'
+// import { useEffect, useState } from 'react'
+// import NavLink from '../components/navLink'
 
-const client = require('contentful').createClient({
-    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
-  })
+// const client = require('contentful').createClient({
+//     space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+//     accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+// })
   
-  function Nav() {
-    async function fetchEntries() {
-      const entries = await client.getEntries({
-        content_type: 'navLink'
-      })
-      if (entries.items) return entries.items
-      console.log(`Error getting Entries for ${contentType.name}.`)
-    }
+// function Nav() {
+//     async function fetchEntries() {
+//       const entries = await client.getEntries({
+//         content_type: 'navLink'
+//       })
+//       if (entries.items) return entries.items
+//       console.log(`Error getting Entries for ${contentType.name}.`)
+//     }
   
-    const [navLinks, setNavLinks] = useState([])
+//     const [navLinks, setNavLinks] = useState([])
   
-    useEffect(() => {
-      async function getNavLink() {
-        const allNavLink = await fetchEntries()
-        setNavLinks([...allNavLink])
-      }
-      getNavLink()
-    }, [])
+//     useEffect(() => {
+//       async function getNavLink() {
+//         const allNavLink = await fetchEntries()
+//         setNavLinks([...allNavLink])
+//       }
+//       getNavLink()
+//     }, [])
   
-    return (
-      <>
-        <div className='navLinkWrapper'>
-          {navLinks.length > 0
-            ? navLinks.map((x) => (
-                <NavLink
-                  link={x.fields.link}
-                  key={x.fields.link}
-                />
-              ))
-            : null}
-        </div>
-        <style jsx>{`
-            .navLinkWrapper {
-              display: flex;
-              justify-content: space-between;
-            }
-          `}</style>
-      </>
-    )
-  }
+//     return (
+//       <>
+//         <div className='navLinkWrapper'>
+//           {navLinks.length > 0
+//             ? navLinks.map((x) => (
+//                 <NavLink
+//                   link={x.fields.link}
+//                   key={x.fields.link}
+//                 />
+//               ))
+//             : null}
+//         </div>
+//         <style jsx>{`
+//             .navLinkWrapper {
+//               display: flex;
+//               justify-content: space-between;
+//             }
+//           `}</style>
+//       </>
+//     )
+//   }
   
-  export default Nav
+//   export default Nav
