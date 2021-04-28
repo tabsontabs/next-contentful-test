0c315f832383124a07b6f3302f752edeca4c7fae
tabsontabs <tabithapoeze@gmail.com>
2021-04-28 17:19:47 -0400

    more nav testing

--- components/nav.js
+++ components/nav.js
@@ -48,3 +48,54 @@
 //   }
   
 //   export default Nav
+
+import { useEffect, useState } from 'react'
+import NavLink from '../components/navLink'
+
+const client = require('contentful').createClient({
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+})
+  
+function Nav() {
+    // async function fetchEntries() {
+    //   const entries = await client.getEntries({
+    //     content_type: 'navLink'
+    //   })
+    //   if (entries.items) return entries.items
+    //   console.log(`Error getting Entries for ${contentType.name}.`)
+    // }
+  
+    // const [navLinks, setNavLinks] = useState([])
+  
+    // useEffect(() => {
+    //   async function getNavLink() {
+    //     const allNavLink = await fetchEntries()
+    //     setNavLinks([...allNavLink])
+    //   }
+    //   getNavLink()
+    // }, [])
+  
+    // return (
+    //   <>
+    //     <div className='navLinkWrapper'>
+    //       {navLinks.length > 0
+    //         ? navLinks.map((x) => (
+    //             <NavLink
+    //               link={x.fields.link}
+    //               key={x.fields.link}
+    //             />
+    //           ))
+    //         : null}
+    //     </div>
+    //     <style jsx>{`
+    //         .navLinkWrapper {
+    //           display: flex;
+    //           justify-content: space-between;
+    //         }
+    //       `}</style>
+    //   </>
+    // )
+  }
+  
+  export default Nav
