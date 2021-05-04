34b43dd0ce5f086e6a077cb0eab9c367032ce3f0
tabsontabs <tabithapoeze@gmail.com>
2021-05-03 20:27:31 -0400

    contact variable update

--- pages/api/contactAPI.js
+++ pages/api/contactAPI.js
@@ -1,6 +1,6 @@
 export default function (req, res) {
 
-    const PASSWORD = process.env.password
+    const PASSWORD = process.env.NEXT_PUBLIC_EMAIL_PASSWORD
 
     let nodemailer = require('nodemailer')
 
