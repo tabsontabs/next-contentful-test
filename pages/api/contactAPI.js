52764acd705c2a0e75ab0a639ff3c6f4cc847d9e
tabsontabs <tabithapoeze@gmail.com>
2021-05-05 17:17:14 -0400

    sendgrid test 3

--- pages/api/contactAPI.js
+++ pages/api/contactAPI.js
@@ -29,9 +29,6 @@ export default async function (req, res) {
     //         console.log(info)
     // })
 
-
-
-
     const sgMail = require('@sendgrid/mail');
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
