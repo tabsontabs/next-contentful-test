e1190cbd6457f818c3896315fba363935b69d6f8
tabsontabs <tabitha.poeze@gmail.com>
2022-04-22 10:14:01 -0400

    add every heart tile

--- pages/brands.js
+++ pages/brands.js
@@ -5,6 +5,8 @@ import { createClient } from 'contentful'
 import styles from '../styles/BrandsPage.module.css'
 import Newfooter from '../components/newfooter';
 
+import Image from 'next/image'
+
 export async function getStaticProps() {
 
   const client = createClient({
@@ -43,6 +45,88 @@ export default function BrandsPage({ brandsPage }) {
               ))
             ))
           }
+          <div className="container everyHeart_container">
+          
+            <div className='brandContent'>
+              <a>
+              <div className='brandImageContainer everyHeart_imageContainer'>
+              <Image 
+                src='/eh_tile.png'
+                width='2328'
+                height='2910'
+                className='brandsPageFeaturedImage'
+                alt='every heart tile'
+              />
+              </div>
+              <div className='brandLogoContainer everyHeart_logoContainer'>
+                <Image 
+                  src='/eh_logo_white_new.png'
+                  width='509'
+                  height='84'
+                  className="brandsPageBrandLogo"
+                  alt='every heart logo'
+                />
+              </div>
+              <button className='brandSeeMore everyHeart_comingSoon'>Coming Soon</button>
+              </a>
+            </div>
+        <style jsx>{`
+          @media only screen and (min-width: 1000px) {
+            .Colours_logoContainer {
+              width: 45%;
+            }
+          }
+          .Colours_logoContainer {
+            width: 50%;
+          }
+          .Vitaly_logoContainer {
+            width: 25%;
+          }
+          
+          .brandImageContainer div {
+            border: 1px solid transparent;
+          }
+          
+          .brandImageContainer div:hover {
+            border: 1px solid white;
+          }
+          
+          .brandLogoContainer {
+            width: 30%;
+            height: 2rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-top: 18px;
+          }
+          
+          .brandSeeMore {
+            font-size: 16px;
+          }
+          
+          .everyHeart_logoContainer {
+            width: 45%;
+          }
+
+          .everyHeart_container:hover, .everyHeart_container .brandContent:hover, .everyHeart_container a:hover, .everyHeart_comingSoon:hover {
+            cursor: not-allowed;
+          }
+          
+          @media only screen and (min-width: 1600px) {
+            .brandLogoContainer {
+              height: 4rem;
+            }
+          
+            .brandSeeMore {
+              font-size: 24px;
+              padding-top: 1rem;
+            }
+          }
+          
+         
+          }
+        `}</style>
+      </div>
           </div>
         </section>
         <Newfooter/>
